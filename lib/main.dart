70ad520618a1906c86bd3b1f933940355957afd8
Justin <jusin1545@gmail.com>
2023-01-28 14:42:22 +0900

    Flutter AssetImage(이미지 출력 위젯)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_string_escapes
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,39 +12,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      // home: const JusinHomePage(),
-      home: const MyWidget(
-        name: 'Justin',
-        title: 'Title',
-      ),
-    );
+        // title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        // home: const JusinHomePage(),
+        home: const _ImageHomePage());
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({
-    super.key,
-    required this.name,
-    required this.title,
-  });
-
-  final String name;
-  final String title;
+class _ImageHomePage extends StatelessWidget {
+  const _ImageHomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    /// assets image
+    /// Copy Relative Path -> assets\image\model3.jpg
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
+
+      /// 화면 overflow 발생시 사용하면 화면이 자동으로 스크롤 된다.
+      /// pubspec.yami 파일에 이미지 폴더 경로를 입력해 준다.
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Hello $name World'),
-            Text('Hello $title World'),
+            Image.asset('assets/image/model3.jpg'),
+            Image.asset('assets/image/Tesla_fsd.gif'),
+            Image.asset('assets/image/Elon.jpg'),
           ],
         ),
       ),
